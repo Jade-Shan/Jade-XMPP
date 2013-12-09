dd33fcdf0f9e3b6b137d53a3f0b7e5f179860cbf
Jade Shan <evokeralucard@gmail.com>
2013-12-09 18:10:58 +0800

    finish IQ

--- src/main/scala/modle/stanzas.scala
+++ src/main/scala/modle/stanzas.scala
@@ -34,28 +34,32 @@ class Stream(override val to: String, private[this] var props: Map[String, Any],
 
 }
 
-// class StanzasStream(val to: String) extends Packet {
-// 
-// 	def toXML = <stream:stream version="1.0" xmlns="jabber:client" 
-// 		xmlns:stream="http://etherx.jabber.org/streams"
-// 		to="{to}"></stream:stream>
-// 
-// }
  
-// abstract class IQ extends Packet {
-// 	var msgType = IQ.Type.GET
-// 
-// 	def getChildElementXML(): Node
-// 
-// }
-// 
-// object IQ extends Logging {
-// 
-// 	object Type extends Enumeration { 
-// 		val GET = Value("get")
-// 		val SET = Value("set")
-// 		val RESULT = Value("result")
-// 		val ERROR = Value("error") 
-// 	}
-// 
-// }
+abstract class IQ(private[this] val mType: IQ.Type.Value, val id: String, 
+	override val from: String, override val to: String, 
+	override val error: XMPPError, private[this] var props: Map[String, Any],
+	private[this] var pktExts: List[PacketExtension]) extends Packet(
+	null, null, from, to, error, props, pktExts)
+{
+
+	val msgType = if (null == mType) IQ.Type.GET else mType
+
+	override def addAttributeToXML(node: Elem): Elem = {
+		super.addAttributeToXML(node) % Attribute(None, "id", Text(id), 
+			Attribute(None, "type", Text(msgType.toString), Null))
+	}
+
+	def nodeXML(fillContent: NodeBuffer): Elem = <iq>{fillContent}</iq> 
+
+}
+
+object IQ extends Logging {
+
+	object Type extends Enumeration { 
+		val GET = Value("get")
+		val SET = Value("set")
+		val RESULT = Value("result")
+		val ERROR = Value("error") 
+	}
+
+}
--- src/test/scala/model/StanzasTest.scala
+++ src/test/scala/model/StanzasTest.scala
@@ -12,6 +12,15 @@ import org.scalatest.junit.JUnitRunner
 import org.scalatest.FunSuite
 import org.junit.runner.RunWith
 
+
+class TestIQ(private[this] val mType: IQ.Type.Value, override val id: String, 
+	override val from: String, override val to: String, 
+	override val error: XMPPError, private[this] var props: Map[String, Any],
+	private[this] var pktExts: List[PacketExtension]) extends IQ ( mType, id, 
+	from, to, error, props, pktExts)
+{
+}
+
 @RunWith(classOf[JUnitRunner])
 class StanzTest extends FunSuite {
 
@@ -23,6 +32,12 @@ class StanzTest extends FunSuite {
 	val pktProps: Map[String, Any] = Map("version" -> 50, "name" -> "account",
 		"balance" -> 55.35)
 
+	val appExtList = new TestSub("test1") :: new TestSub("test2") :: 
+	new TestSub("test3") :: Nil
+
+	val err = new XMPPError( XMPPError.Condition.interna_server_error, 
+			"Oops", appExtList)
+
 	test("Test-Stream-toXML") {
 		assert(
 			<stream:stream xmlns="jabber:client" to="to@google.com" version="1.0" xmlns:stream="http://etherx.jabber.org/streams"><testSub>test1</testSub><testSub>test2</testSub><testSub>test3</testSub></stream:stream> ==
@@ -31,5 +46,11 @@ class StanzTest extends FunSuite {
 			<stream:stream xmlns="jabber:client" to="to@google.com" version="1.0" xmlns:stream="http://etherx.jabber.org/streams"><properties xmlns="http://www.jivesoftware.com/xmlns/xmpp/properties"><property><name>version</name><value code="50">integer</value></property><property><name>name</name><value code="account">string</value></property><property><name>balance</name><value code="55.35">double</value></property></properties><testSub>test1</testSub><testSub>test2</testSub><testSub>test3</testSub></stream:stream> ==
 			XML.loadString(new Stream("to@google.com", pktProps, pkExtList).toXML.toString))
 	}
+
+	test("Test-IQ") {
+		assert(<iq type="get" id="Os3j-5796" from="from@gmail.com" to="to@gmail.com"><properties xmlns="http://www.jivesoftware.com/xmlns/xmpp/properties"><property><name>version</name><value code="50">integer</value></property><property><name>name</name><value code="account">string</value></property><property><name>balance</name><value code="55.35">double</value></property></properties><testSub>test1</testSub><testSub>test2</testSub><testSub>test3</testSub></iq> ==
+			XML.loadString(new TestIQ(IQ.Type.GET, "Os3j-5796", "from@gmail.com", 
+				"to@gmail.com", err, pktProps, pkExtList).toXML.toString))
+		}
 
 }
