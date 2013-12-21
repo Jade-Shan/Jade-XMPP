5e6f5a903fd6e5f0ef2af87654dd3c5f968e2072
Jade Shan <evokeralucard@gmail.com>
2013-12-22 01:10:19 +0800

    add app

---
+++ src/main/scala/Example.scala
@@ -0,0 +1,10 @@
+object Example extends App {
+ 
+    if (args.length > 1)
+        println("Say: " + args(0) + " " + args(1))
+    else if (args.length > 0)
+        println("Say: " + args(0))
+    else
+        println("Say nothing")
+ 
+}
