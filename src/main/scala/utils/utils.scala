1f114a55e87ddb450c8a29dff203ae7bf8d5f98c
Jade Shan <evokeralucard@gmail.com>
2013-12-26 02:00:58 +0800

    create XMLLoader failed

--- src/main/scala/utils/utils.scala
+++ src/main/scala/utils/utils.scala
@@ -16,3 +16,75 @@ class XMPPException(val msg: String, val cause: Throwable)
 	}
 
 }
+
+
+
+
+
+
+
+
+// abstract class MessageReader extends Actor {
+// 	val logger = MessageReader.logger
+// 	val reader: Reader
+// 	val processer: Actor
+// 	private[this] var xmlLoader: XMLLoader = null
+// 
+// 	var keepReading = true
+// 
+// 	def init() {
+// 		logger.debug("MessageReader init ..."); 
+// 		xmlLoader = new XMLLoader(reader)
+// 		processer.start() 
+// 	}
+// 
+// 	def close() { keepReading = false }
+// 
+// 	def act() {
+// 		logger.debug("MessageReader start ...")
+// 		while (keepReading) processer ! xmlLoader.fillMsg()
+// 	}
+// 
+// }
+// 
+// object MessageReader  extends Logging { }
+// 
+// class XMLLoader(val reader: Reader) {
+// 	val buffSize = 8 * 1024
+// 	private[this] var buffer = new Array[Char](buffSize)
+// 	private[this] var start = 0 
+// 	private[this] var curr = 0 
+// 
+// 	import XMLLoader.MsgStat
+// 	// 当前消息读取的完整状态
+// 	private[this] var status: MsgStat.Value = MsgStat.INIT
+// 	private[this] val msg: StringBuffer = new StringBuffer
+// 
+// 	/* clean message ready for a new stanze */
+// 	private[this] def resetMsg() { msg setLength 0; status = MsgStat.INIT }
+// 
+// 	def fillMsg(): String = {
+// 		var len = 0
+// 		while (len != -1 && status != MsgStat.CLOSE) {
+// 			len = reader.read(buffer, 0, buffSize)
+// 			for (i <- 0 until len) { msg.append(buffer(i)) }
+// 			checkMsg(msg.toString)
+// 		}
+// 		msg.toString
+// 	}
+// 
+// 	private[this] def checkMsg(str: String) {
+// 		// TODO: check message complate
+// 		status = MsgStat.CLOSE
+// 	}
+// 
+// }
+// 
+// object XMLLoader {
+// 	object MsgStat extends Enumeration {
+// 		val INIT,  // 等待标签开始
+// 		OPEN,  // 等待标签结束
+// 		XML,  // 等待标签结束
+// 		CLOSE = Value // 标签结束，已经是一条完整的消息
+// 	}
+// }
