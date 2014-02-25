75e5e5b73db8dfe01578c2b3ebe52fe2dbc504f9
Jade Shan <evokeralucard@gmail.com>
2014-02-26 00:17:33 +0800

    update

--- src/main/scala/handler/coreHandler.scala
+++ src/main/scala/handler/coreHandler.scala
@@ -10,8 +10,7 @@ class StreamHandler(conn: Connection) extends MsgHandler with Logging {
 
 	def canProcess(elem: Elem): Boolean = {
 		elem.namespace == """http://etherx.jabber.org/streams""" && 
-			elem.label == """stream""" && 
-			elem.prefix == """stream"""
+			elem.label == """stream""" && elem.prefix == """stream"""
 	}
 
 	def process(elem: Elem) {
