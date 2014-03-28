fb44eb59d490e0964fa4c739d603171a930be9de
Jade Shan <evokeralucard@gmail.com>
2014-03-28 10:54:13 +0800

    update

--- build.sh
+++ build.sh
@@ -29,10 +29,11 @@ do
 			ctags -R src --exclude=target --exclude=vendor
 			# mvn clean scala:compile scala:testCompile resources:resources resources:testResources surefire:test
 			# mvn clean compile test-compile resources:resources resources:testResources surefire:test
+			  mvn clean compile test-compile resources:resources resources:testResources surefire:test -Dtest=PacketTest
 			# mvn clean compile test-compile resources:resources resources:testResources surefire:test -Dtest=IOTest
 			# mvn clean compile test-compile resources:resources resources:testResources surefire:test -Dtest=MessageProcesserTest 
 			# mvn clean compile test-compile resources:resources resources:testResources surefire:test -Dtest=MessageProcesserLoderTest 
-			mvn clean compile test-compile resources:resources resources:testResources surefire:test -Dtest=LoginTest
+			# mvn clean compile test-compile resources:resources resources:testResources surefire:test -Dtest=LoginTest
 			;;
 		r)
 			mvn resources:resources scala:run -Dlauncher=foo
